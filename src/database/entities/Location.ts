44fdf097b6663017ccccb448c976f3dee70b0bda
Carlos Barros <carlos_dbs@hotmail.com>
2023-08-09 11:44:56 -0300

    feat(Location.ts):creates entity

---
+++ src/database/entities/Location.ts
@@ -0,0 +1,11 @@
+class Location {
+  public latitude: string;
+  public longitude: string;
+
+  constructor(latitude: string, longitude: string){
+    this.latitude = latitude;
+    this.longitude = longitude;
+  }
+}
+
+export { Location }
