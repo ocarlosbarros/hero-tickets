a041fd191c22108053abc23f3f5f13cf80d0134e
Carlos Barros <carlos_dbs@hotmail.com>
2023-08-09 12:53:08 -0300

    feat(EventRepositoryMongoose.ts):creates schema, model and implements IEventRepository

---
+++ src/repositories/EventRepositoryMongoose.ts
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+import { IEventRepository } from "./IEventRepository";
+import { Event } from "../database/entities/Event";
+
+const eventSchema = new mongoose.Schema({
+  title: String,
+  date: Date,
+  city: String,
+  description: String,
+  banner: String,
+  categories: [String],
+  flyers: [String],
+  coupons: [String],
+  location: {
+    latitude: String,
+    longitude: String
+  },
+  participants: {
+    type: Array,
+    ref: 'User'
+  },
+  price: {
+    type: Array,
+    ref: 'Price'
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+});
+
+const EventModel = mongoose.model('Event', eventSchema);
+
+class EventRepositoryMongoose implements IEventRepository {
+  async create(event: Event): Promise<Event> {
+    const eventModel = new EventModel(event);
+
+    await eventModel.save();
+    return event;
+  }
+}
+
+export { EventRepositoryMongoose }
