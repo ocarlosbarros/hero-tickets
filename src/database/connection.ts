f13f40f450601f719e0fd6d613c0e913bde477f9
Carlos Barros <carlos_dbs@hotmail.com>
2023-08-09 11:43:24 -0300

    chore:config connection and enviroments variables for database connection

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-import config from "../../config/environment-variables";
+import config from "./config/environment-variables";
 
 export async function connect(){
   try {
