32ad4040dbc34861332dcd5f70dfdea170fb1743
Carlos Barros <carlos_dbs@hotmail.com>
2023-08-09 15:06:26 -0300

    feat(event.routes.ts):adds dependencies in contructor and define post method

--- src/routes/event.routes.ts
+++ src/routes/event.routes.ts
@@ -1,15 +1,23 @@
 import { Router } from 'express';
+import { EventRepositoryMongoose } from '../repositories/EventRepositoryMongoose';
+import { EventController } from '../controllers/EventController';
+import { EventService } from '../services/EventService';
 
 class EventRoutes {
   public router: Router; 
+  private eventController: EventController;
 
   constructor(){
     this.router = Router();
+    const eventRepository = new EventRepositoryMongoose();
+    const eventService = new EventService(eventRepository);
+    this.eventController = new EventController(eventService);
     this.initRoutes();
   };
 
   initRoutes(){
     this.router.get('/');
+    this.router.post('/', this.eventController.create.bind(this.eventController));
   };
 
 }
