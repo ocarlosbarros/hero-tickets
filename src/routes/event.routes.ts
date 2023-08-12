5841e52bac9a362777668fbfc3923eac20ef57ee
Carlos Barros <carlos_dbs@hotmail.com>
2023-08-12 18:10:13 -0300

    feat(event.router.ts):add upload of the files

--- src/routes/event.routes.ts
+++ src/routes/event.routes.ts
@@ -2,24 +2,36 @@ import { Router } from 'express';
 import { EventRepositoryMongoose } from '../repositories/EventRepositoryMongoose';
 import { EventController } from '../controllers/EventController';
 import { EventService } from '../services/EventService';
+import { upload } from '../database/config/multer';
 
 class EventRoutes {
-  public router: Router; 
+  public router: Router;
   private eventController: EventController;
 
-  constructor(){
+  constructor() {
     this.router = Router();
     const eventRepository = new EventRepositoryMongoose();
     const eventService = new EventService(eventRepository);
     this.eventController = new EventController(eventService);
     this.initRoutes();
-  };
+  }
 
-  initRoutes(){
-    this.router.get('/');
-    this.router.post('/', this.eventController.create.bind(this.eventController));
-  };
-
+  initRoutes() {
+    this.router.post(
+      '/',
+      upload.fields([
+        {
+          name: 'banner',
+          maxCount: 1,
+        },
+        {
+          name: 'flyers',
+          maxCount: 3,
+        },
+      ]),
+      this.eventController.create.bind(this.eventController),
+    );
+  }
 }
 
-export { EventRoutes }
+export { EventRoutes };
