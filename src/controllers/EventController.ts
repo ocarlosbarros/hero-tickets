7f8e7630206ab8a6cd49b518573e9907ad289406
Carlos Barros <carlos_dbs@hotmail.com>
2023-08-12 18:11:04 -0300

    refactor(EventController):adds Event entity

--- src/controllers/EventController.ts
+++ src/controllers/EventController.ts
@@ -1,24 +1,24 @@
-import { Request, Response, NextFunction } from "express";
-import { EventService } from "../services/EventService";
+import { Request, Response, NextFunction } from 'express';
+import { Event } from '../database/entities/Event';
+import { EventService } from '../services/EventService';
 
 class EventController {
-  constructor(private eventService: EventService){}
-  async create(request: Request, response: Response, next: NextFunction){
+  constructor(private eventService: EventService) {}
+  async create(request: Request, response: Response, next: NextFunction) {
     try {
-      const event = request.body;
+      const event: Event = request.body;
       const created = await this.eventService.create(event);
 
-      if(!created){
-        return response.status(400).json({ message: 'Bad Request'});
+      if (!created) {
+        return response.status(400).json({ message: 'Bad Request' });
       }
-      return response.status(201).json({ message: 'Evento criado com sucesso!'});
-
+      return response
+        .status(201)
+        .json({ message: 'Evento criado com sucesso!' });
     } catch (error) {
-      next(error);
+      return next(error);
     }
   }
-
-
 }
 
-export { EventController }
+export { EventController };
